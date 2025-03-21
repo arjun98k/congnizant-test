8eb53068fa9b59caaba1991cbabcc74f357e216f
arjun98k <arjunkandekar14@gmail.com>
2025-03-21 18:07:43 +0530

    alternate element print in array

---
+++ src/Easy/PrintAlternate.java
@@ -0,0 +1,13 @@
+package Easy;
+
+public class PrintAlternate {
+    public static void main(String[] args) {
+        int arr[] = {11,22,33,44,55};
+        for (int i = 0; i < arr.length ; i+=2) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+
+
