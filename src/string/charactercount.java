864e9fdd4043bb1c6773c4c04e80bc0b115e4ab4
arjun98k <arjunkandekar14@gmail.com>
2025-04-01 10:54:45 +0530

    character count

--- src/string/charactercount.java
+++ src/string/charactercount.java
@@ -1,8 +1,28 @@
 package string;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class charactercount {
     public static void main(String[] args) {
         String str = "Hello Arjun kandekar";
+        String str1 = new String("akash");
+           str1.concat(" Salekar");
+        System.out.println(str1);
+
+        StringBuilder sb = new StringBuilder("Akshay");
+        sb.append(" salekar");
+        List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
+
+        List<Integer> list2 = list;
+
+        list2.add(12);
+
+        System.out.println(list);
+
+
+        System.out.println(sb.toString());
         int count = 0;
         int space = 0;
         for (int i = 0; i < str.length() ; i++) {
