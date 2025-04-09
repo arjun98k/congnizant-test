7346bcf7246e9f626a18a7d4b9e27e893c837198
arjun98k <arjunkandekar14@gmail.com>
2025-04-09 19:01:33 +0530

    bubble sort

--- src/Sort/BubbleSort.java
+++ src/Sort/BubbleSort.java
@@ -3,6 +3,8 @@ package Sort;
 public class BubbleSort {
     public static void main(String[] args) {
         int arr[] = {3,2,5,1,9,8,7};
+        // Bubble sort
+        // TC N^2
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j <arr.length-i-1 ; j++) {
                 if (arr[j]>arr[j+1]){
