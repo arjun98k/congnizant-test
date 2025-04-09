2b4df6feb97493690e36a82bfb817026ae26ba43
arjun98k <arjunkandekar14@gmail.com>
2025-04-09 20:26:18 +0530

    Selection sort

---
+++ src/Sort/SelectionSort.java
@@ -0,0 +1,21 @@
+package Sort;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = {3,2,4,1,8,7};
+
+        for (int i = 0; i < arr.length -1 ; i++) {
+             int smallest = i;
+            for (int j = i+1; j < arr.length ; j++) {
+                if (arr[smallest]> arr[j]){
+                    smallest = j;
+                }
+            } int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
