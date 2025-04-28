6b9566e39c2cb823b454ef526e2f7e1c7b11dd67
arjun98k <arjunkandekar14@gmail.com>
2025-04-28 23:39:46 +0530

    positive and negative

--- src/Numbers/PositiveAndNegative.java
+++ src/Numbers/PositiveAndNegative.java
@@ -6,7 +6,9 @@ public class PositiveAndNegative {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter number you want to check ");
         int num = sc.nextInt();
+
         if (num > 0){
             System.out.println("Given number is positive:  " + num);
         } else if (num < 0) {
@@ -14,5 +16,6 @@ public class PositiveAndNegative {
         } else {
             System.out.println("Given number is zero");
         }
+        sc.close();
     }
 }
