4fe1c04dad231b4d27593d0dc6879c94aa4f3f0e
arjun98k <arjunkandekar14@gmail.com>
2025-04-30 20:15:35 +0530

    word count inn txt file

---
+++ src/string/Wordcounttxt.java
@@ -0,0 +1,19 @@
+package string;
+
+import java.io.*;
+
+public class Wordcounttxt {
+    public static void main(String[] args) throws IOException {
+        String line;
+        int count = 0;
+        FileReader file = new FileReader("C:\\Users\\Akash\\OneDrive\\Desktop\\Congizant prog\\progem\\src\\string\\ak.txt");
+        BufferedReader br = new BufferedReader(file);
+
+        while ((line = br.readLine()) != null ){
+            String word[] = line.split("\\s+");
+            count += word.length;
+        }
+        System.out.println("the number of word present in given file:  "+ count);
+        br.close();
+    }
+}
