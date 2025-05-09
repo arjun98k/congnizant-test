bf1048eca0f53a30bda4edde83385b303ce1fbcf
arjun98k <arjunkandekar14@gmail.com>
2025-05-10 01:19:46 +0530

    sum of inside digit num

---
+++ src/practice/SumNum.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class SumNum {
+    public static int Sumnumm(int n){
+        int sum = 0;
+        while (n != 0){
+            // get last digit
+            int last = n%10;
+
+            sum = sum + last;
+
+            // remove last digit
+            n = n/10;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println(Sumnumm(123));
+    }
+}
