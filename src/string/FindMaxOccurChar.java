01eca7773bce95f2f6680165d40b1ae3d2d98e9a
arjun98k <arjunkandekar14@gmail.com>
2025-05-09 20:20:52 +0530

    Find max occur char

--- src/string/FindMaxOccurChar.java
+++ src/string/FindMaxOccurChar.java
@@ -1,5 +1,8 @@
 package string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindMaxOccurChar {
 public static int findmax(String str){
     int maxcount = 0;
@@ -23,7 +26,29 @@ public static int findmax(String str){
 
 }
 
+public  static int maxChar(String str1){
+    HashMap<Character, Integer> countcharr = new HashMap<>();
+
+    for (int i = 0; i < str1.length() ; i++) {
+        char ch = str1.charAt(i);
+        countcharr.put(ch,countcharr.getOrDefault(ch,0) +1);
+    }
+    int maxCount = 0;
+    char Maxchar = ' ';
+
+    for (Map.Entry<Character,Integer> entry : countcharr.entrySet()){
+        if (entry.getValue()>maxCount){
+            Maxchar = entry.getKey();
+            maxCount = entry.getValue();
+        }
+    }
+    System.out.println(Maxchar);
+    return maxCount;
+}
+
     public static void main(String[] args) {
-        System.out.println(findmax("geeksforgeeks"));
+
+//    System.out.println(findmax("geeksforgeeks"));
+        System.out.println(maxChar("geeksforgeeks"));
     }
 }
